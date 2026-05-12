916c27fde4b0b1bf35f707610149a287748f6ec8
SloshyDev <devlnicanorl@gmail.com>
2026-05-12 00:53:06 -0600

    fix: reorder methodology tab elements and improve click handling logic

--- tm-ECM.php
+++ tm-ECM.php
@@ -343,9 +343,9 @@ $current_lang = ICL_LANGUAGE_CODE; ?>
 <!-- METODOLOGY -->
 <section class='my-8'>
     <div class="flex  justify-between">
-
-        <div id="tab1" class="border-t-[4rem] border-t-gray-50 dark:border-t-gray-900 border-solid border-l-[5rem] border-l-transparent border-r-0 border-r-transparent h-0 w-8/12 2xl:w-4/5"></div><h1 id='metTitle' style="width: 23vw;" class='text-gray-50  bg-[#4b626e] py-4 text-3xl 2xl:text-5xl isBlack m-auto ml-4 md:ml-auto mb-0 text-center' style="margin-bottom: 0 !important;"><?php _e("TRANSECTS", 'dm') ?></h1>
-        <h1 id='t5' style="width: 23vw;background: #CACFD3;" class='text-gray-50 py-4 text-3xl 2xl:text-5xl isBlack m-auto ml-4 md:ml-auto mb-0 text-center cursor-pointer' style="margin-bottom: 0 !important;"><?php _e("METHODOLOGY PCU", 'dm') ?></h1>
+<h1 id='metTitle' style="width: 23vw;" class='text-gray-50  bg-[#4b626e] py-4 text-3xl 2xl:text-5xl isBlack m-auto ml-4 md:ml-auto mb-0 text-center' style="margin-bottom: 0 !important;"><?php _e("TRANSECTS", 'dm') ?></h1>
+        <div id="tab1" class="border-t-[4rem] border-t-gray-50 dark:border-t-gray-900 border-solid border-l-[5rem] border-l-transparent border-r-0 border-r-transparent h-0 w-8/12 2xl:w-4/5"></div>
+        <h1 id='t5' style="width: 23vw;background: #CACFD3;" class='text-gray-50 py-4 text-3xl 2xl:text-5xl isBlack m-auto ml-4 md:ml-auto mb-0 text-center cursor-pointer hidden' style="margin-bottom: 0 !important;"><?php _e("METHODOLOGY PCU", 'dm') ?></h1>
         <div id="tab2" style="border-left-color: #cacfd3;"  class="border-t-[4rem] border-t-gray-50 dark:border-t-gray-900 border-solid border-l-[5rem] border-l-transparent border-r-0 border-r-transparent h-0 w-8/12 2xl:w-4/5 hidden"></div>
     </div>
     <img id='met1' class='metImg' src="<?php the_field('met1'); ?>" alt="">
@@ -1033,10 +1033,12 @@ $current_lang = ICL_LANGUAGE_CODE; ?>
 
     $('#tab1').css('border-left-color', '#4b626e !important');
 
-    function setMethodologyState(id, text) {
+    $('.metButton, #t5').click(function() {
+        let text = $(this).hasClass('metButton') ? $(this).find('.metText').text() : $(this).text();
         let tab1 = document.getElementById('tab1');
         let tab2 = document.getElementById('tab2');
         let t5 = document.getElementById('t5');
+        let id = $(this).attr('id');
 
         if (id === 't3' || id === 't5') {
             tab2.classList.remove('hidden');
@@ -1049,25 +1051,10 @@ $current_lang = ICL_LANGUAGE_CODE; ?>
             $('#tab1').css('border-top-color', '#cacfd300 !important');
             t5.classList.add('hidden');
         }
-
         $('#metTitle').text(text);
         $('.metImg').addClass('hidden');
         $('#me' + id).removeClass('hidden');
-    }
-
-    $('.metButton').click(function() {
-        let id = $(this).attr('id');
-        let text = $(this).find('.metText').text();
-        setMethodologyState(id, text);
     });
-
-    $('#t5').click(function() {
-        let id = 't5';
-        let text = $(this).text().trim();
-        setMethodologyState(id, text);
-    });
-
-    if()
 
     function ecologicalCS() {
         $("#sponsorsMonitoring").slick({
